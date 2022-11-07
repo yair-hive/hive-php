16586f2b9407cd73ef53421b58b794eeb652d6fb
yair man <yairman2429@gmail.com>
2022-11-07 20:15:11 +0200

    עיצוב

--- edit_map.php
+++ edit_map.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="front_end/style.css">
     <link rel="stylesheet" href="front_end/viselect.css">
+    <link rel="stylesheet" href="front_end/style.css">
     <script src="front_end/lib/jquery.min.js"></script>
     <script src="front_end/lib/viselect.cjs.js"></script>
     <script src="front_end/script.js"></script>
@@ -30,7 +30,9 @@
     <title> hive | </title>
 </head>
 <body>
-    <div id="topBar"></div>
+    <div id="topBar">
+        <a href="http://localhost/hive-php/maps.php"> map </a>
+    </div>
     <div id="mainBord"></div>
     <div id="mneu">
         <div id='sub' class='sub'> submit </div>
