ad23d69cef598fa1f0eb44180610e74fc7383af8
yair man <yairman2429@gmail.com>
2022-11-14 11:51:29 +0200

    סירתי טעינה אוטומטית

--- front_end/scripts/elements.js
+++ front_end/scripts/elements.js
@@ -9,14 +9,19 @@ export const add_map = (map)=>{
     map_ele.classList.add('map')
     for(var rowsCounter = 1; rowsCounter <= map.rows_number; rowsCounter++){
         for(var colsCounter = 1; colsCounter <= map.columns_number; colsCounter++){
+            var cell_cont = document.createElement('div')
             var cell = document.createElement('div')
-            cell.setAttribute('row', rowsCounter)
-            cell.setAttribute('col', colsCounter)
+            cell_cont.classList.add(`row-${rowsCounter}`)
+            cell_cont.classList.add(`col-${colsCounter}`)
+            cell_cont.classList.add('cell_cont')
             cell.classList.add(`row-${rowsCounter}`)
             cell.classList.add(`col-${colsCounter}`)
+            cell.setAttribute('row', rowsCounter)
+            cell.setAttribute('col', colsCounter)
             cell.classList.add('cell')
             cell.classList.add('selectable')
-            map_ele.appendChild(cell)
+            cell_cont.append(cell)
+            map_ele.appendChild(cell_cont)
         }
     }
     map_ele.style.setProperty('--map-rows', map.rows_number)
@@ -26,20 +31,22 @@ export const add_map = (map)=>{
 }
 export const add_seats = (seats)=>{
     for(let seat of seats){
-        var cell = document.querySelector(`.row-${seat.row_num}.col-${seat.col_num}`)
+        var cell = document.querySelector(`.cell_cont.row-${seat.row_num}.col-${seat.col_num}`)
+        var seat_ele = document.createElement('div')
+        seat_ele.classList.add(`row-${seat.row_num}`)
+        seat_ele.classList.add(`col-${seat.col_num}`)
         var num_box = document.createElement('div')
         var name_box = document.createElement('div')
         num_box.classList.add('num_box')
         name_box.classList.add('name_box')
-        cell.classList.remove('cell')
-        cell.classList.remove('selectable')
-        cell.classList.add('seat')
+        seat_ele.classList.add('seat')
         $(name_box).attr('seat_id', seat.id)
         $(num_box).attr('seat_id', seat.id)
         $(name_box).attr('guest_id', seat.guest_id)
         $(num_box).text(seat.seat_number)
-        cell.append(num_box)
-        cell.append(name_box)
+        seat_ele.append(num_box)
+        seat_ele.append(name_box)
+        cell.replaceChildren(seat_ele)
     }
 }
 export const add_guest = (guests)=>{
