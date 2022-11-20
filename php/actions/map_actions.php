f65dcfbbafd004459e1bac7f62c061e25882ee3f
yair man <yairman2429@gmail.com>
2022-11-20 17:40:26 +0200

    בדר בפונקציות

--- php/actions/map_actions.php
+++ php/actions/map_actions.php
@@ -1,4 +1,21 @@
 <?php
+$map_actions['create'] = function(){
+    if(allowed('writing')){
+        $map_name = $_POST['map_name'];
+        $rows_number = $_POST['rows_number'];
+        $columns_number = $_POST['columns_number']; 
+        if(!empty($map_name) && !empty($rows_number) && !empty($columns_number)){                
+            $query_string = "INSERT INTO maps(map_name, rows_number, columns_number) VALUES('{$map_name}', '{$rows_number}', '{$columns_number}')";
+            db_post($query_string);
+        }else{
+            $respons['msg'] = 'faild empty';
+            print_r(json_encode($respons));;
+        }
+    }else{
+        $respons['msg'] = 'dinaid';
+        print_r(json_encode($respons));
+    }
+};
 $map_actions['get_all'] = function(){
     if(allowed("reading")){    
         $query_string = "SELECT map_name FROM maps";
@@ -14,23 +31,6 @@ $map_actions['get'] = function(){
         global $connection;        
         $query_string = "SELECT * FROM maps WHERE map_name='{$map_name}'";
         db_get($query_string);
-    }else{
-        $respons['msg'] = 'dinaid';
-        print_r(json_encode($respons));
-    }
-};
-$map_actions['create'] = function(){
-    if(allowed('writing')){
-        $map_name = $_POST['map_name'];
-        $rows_number = $_POST['rows_number'];
-        $columns_number = $_POST['columns_number']; 
-        if(!empty($map_name) && !empty($rows_number) && !empty($columns_number)){                
-            $query_string = "INSERT INTO maps(map_name, rows_number, columns_number) VALUES('{$map_name}', '{$rows_number}', '{$columns_number}')";
-            db_post($query_string);
-        }else{
-            $respons['msg'] = 'faild empty';
-            print_r(json_encode($respons));;
-        }
     }else{
         $respons['msg'] = 'dinaid';
         print_r(json_encode($respons));
