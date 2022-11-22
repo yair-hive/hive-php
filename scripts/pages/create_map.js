71adbbc46442032fd40e66e4e34abc47366f0a00
yair man <yairman2429@gmail.com>
2022-11-22 13:51:41 +0200

    תיקון קטן

--- scripts/pages/create_map.js
+++ scripts/pages/create_map.js
@@ -1,1 +1,6 @@
-document.getElementById('create_map').addEventListener('click', ()=>{map.create().then(()=>{window.location.replace('http://localhost/hive-php/html/maps.html')})})
+import { map } from '../api/map.js'
+document.getElementById('create_map').addEventListener('click', ()=>{
+    map.create().then(()=>{
+        window.location.replace('http://localhost/hive-php/html/maps.html')
+    })
+})
