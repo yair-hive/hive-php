19c953bb345d8621afee7d5f2042ce796da43767
yair man <yairman2429@gmail.com>
2022-11-22 14:23:00 +0200

    קצת סדר

--- scripts/pages/maps.js
+++ scripts/pages/maps.js
@@ -1,5 +1,4 @@
 import { map } from "../api/map.js"
-import "../lib/jquery.min.js"
 var maps_list = document.getElementById('maps_list')
 map.get_all()
 .then((respons)=>{
@@ -13,9 +12,12 @@ map.get_all()
 .then((maps)=>{
     if(maps){
         for(let map of maps){
-            var li = $('<li>')
-            .append($('<a>').attr('href', `edit_map.html?map_name=${map}`).text(map))
-            $(maps_list).append(li)
+            var li = document.createElement('li')
+            var a = document.createElement('a')
+            a.setAttribute('href', `edit_map.html?map_name=${map}`)
+            a.textContent = map
+            li.append(a)
+            maps_list.append(li)
         }
     }
 })
