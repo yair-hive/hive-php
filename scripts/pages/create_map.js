067ff47ee3b66ecc882bd876945b3b83c609a9be
yair man <yairman2429@gmail.com>
2022-12-13 13:36:45 +0200

    ננג

--- scripts/pages/create_map.js
+++
@@ -1,6 +0,0 @@
-import api from '../api/api.js'
-document.getElementById('create_map').addEventListener('click', ()=>{
-    api.map.create().then(()=>{
-        window.location.replace('http://localhost/hive-php/html/maps.html')
-    })
-})
--- scripts/pages/maps.js
+++ scripts/pages/maps.js
@@ -1,6 +1,33 @@
 import { openPopUp } from "../elements.js"
 import api from '../api/api.js'
+import popUp from "../popUp.js"
 var maps_list = document.getElementById('maps_list')
+
+function add_create_map_form(){
+    return `<form id='create_map_form'>
+        <label for="map_name"> שם המפה </label>
+        <br />
+        <input type='text' name='map_name'>  
+        <br />
+        <label for="rows_number"> מספר שורות </label>
+        <br />
+        <input type='text' name='rows_number'>
+        <br />
+        <label for="columns_number"> מספר טורים </label>
+        <br />
+        <input type='text' name='columns_number'> 
+        <br /> 
+    <form>
+    <div id='create_map' class='hive-button'> צור </div>`
+}
+var create_map_pop_up = new popUp('צור מפה', add_create_map_form())
+create_map_pop_up.onOpen = function(pop_up){
+    document.getElementById('create_map').addEventListener('click', ()=>{
+        api.map.create()
+        pop_up.close()
+    })
+}
+document.getElementById('add_map').addEventListener('click', create_map_pop_up.open)
 api.map.get_all()
 .then((respons)=>{
     if(respons.msg != 'ok'){
