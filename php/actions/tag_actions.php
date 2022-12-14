71dc7cd061fad44a62e5871faa2846acbc3b7491
yair man <u0504171788@gmail.com>
2022-12-14 20:43:28 +0200

    שלא יהיה אפשר להוסיף יותר מבקשה אחת

--- php/actions/tag_actions.php
+++ php/actions/tag_actions.php
@@ -1,10 +1,15 @@
 <?php
 $tags_actions['add_request'] = function(){
     global $NEW_POST;
+    global $connection;
     $guest_id = $NEW_POST['guest_id'];
     $request_id = $NEW_POST['tag_id'];
-    $query_string = "INSERT INTO guests_requests(guest, request) VALUES('{$guest_id}', '{$request_id}')";
-    db_post($query_string);
+    $query_string = "SELECT * FROM guests_requests WHERE guest = '{$guest_id}' AND request = '{$request_id}";
+    $result = mysqli_query($connection, $query_string);
+    if(mysqli_num_rows($result) == 0){
+        $query_string = "INSERT INTO guests_requests(guest, request) VALUES('{$guest_id}', '{$request_id}')";
+        db_post($query_string);
+    }
 };
 $tags_actions['get_requests'] = function(){
     global $NEW_POST;
