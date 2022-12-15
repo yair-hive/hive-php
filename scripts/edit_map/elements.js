b6f4db3b9e8696182081396d541059d8c47afd7c
yair man <u0504171788@gmail.com>
2022-12-16 01:30:33 +0200

    סדר בניקוד

--- scripts/edit_map/elements.js
+++ scripts/edit_map/elements.js
@@ -111,7 +111,7 @@ export const add_guests = (guests)=>{
                     guests[c].color = group.color
                     guests[c].group_id = guests[c].guest_group
                     guests[c].guest_group = group.group_name
-                    guests[c].score = group.score
+                    guests[c].score = Number(guests[c].score) + Number(group.score)
                 }
             }
         }
